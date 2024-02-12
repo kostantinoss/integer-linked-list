0b113b6f4b207eaa8a1055d91bb93369d3dfafe8
Konstantinos Chondralis <khondralis@gmail.com>
2024-02-12 09:27:52 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
+
+int main() {
+    clock_t start, stop;
+    double cpu_time;
+    
+    List *list;
+    list = createList();
+    
+    int limit = 100000000;
+    
+    for (int i = 0; i < limit; i++) {
+        append(list, i);   
+    }
+    
+    start = clock();
+    printf("list[%d] = %d\n", limit - 1, get(list, limit - 1));
+    
+    stop = clock();
+    
+    cpu_time = ((double)(stop - start))/CLOCKS_PER_SEC;
+    printf("cpu time = %.4f sec\n", cpu_time);
+    // printList();
+    
+    return 0;
+}
